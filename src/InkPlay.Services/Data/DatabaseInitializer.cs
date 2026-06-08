9cd793560cca1c2eafc8590ec1294367f21c6f3e
Maple <timemapleling@gmail.com>
2026-06-08 16:38:50 +0800

    perf: 添加数据库索引优化查询
    - Documents 添加 Type 索引，优化按类型过滤查询
    - Conversations 添加 CreatedAt 索引，优化排序查询

--- src/InkPlay.Services/Data/DatabaseInitializer.cs
+++ src/InkPlay.Services/Data/DatabaseInitializer.cs
@@ -6,11 +6,13 @@ public static class DatabaseInitializer
     {
         db.Projects.EnsureIndex(x => x.UpdatedAt);
         db.Documents.EnsureIndex(x => x.ProjectId);
+        db.Documents.EnsureIndex(x => x.Type);
         db.Documents.EnsureIndex(x => x.SortOrder);
         db.Characters.EnsureIndex(x => x.ProjectId);
         db.Relationships.EnsureIndex(x => x.ProjectId);
         db.WorldSettings.EnsureIndex(x => x.ProjectId);
         db.Conversations.EnsureIndex(x => x.ProjectId);
+        db.Conversations.EnsureIndex(x => x.CreatedAt);
         db.GlossaryEntries.EnsureIndex(x => x.ProjectId);
         db.DocumentVersions.EnsureIndex(x => x.DocumentId);
         db.DocumentVersions.EnsureIndex(x => x.SnapshotAt);
